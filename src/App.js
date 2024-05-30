9448fcca360a12179f9b67c8631d112b3fc65322
Liubov Alieksieieva <liubovalieksieieva@gmail.com>
2024-05-30 15:25:11 +0300

    commit 3 start app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Deploy react app on Github</p>
+        <div>Hello world </div>
         <a
           className="App-link"
           href="https://reactjs.org"
